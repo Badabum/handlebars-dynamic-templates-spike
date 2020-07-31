a6de8685bc79efb306429c030d968d7683d99b54
Badabum <ihorkorotenko@gmail.com>
2020-07-31 08:27:16 +0300

    Updates benchmarking code

--- Spikes.Handlebars/Benchmark.cs
+++ Spikes.Handlebars/Benchmark.cs
@@ -17,23 +17,7 @@ namespace Spikes.Handlebars
         public void Setup()
         {
             _templateContent = File.ReadAllText(TemplatePath);
-            HandlebarsDotNet.Handlebars.RegisterHelper("date", (writer, context, args) =>
-            {
-                DateTime dt = (DateTime)args[0];
-                var format = args[1] as string;
-                writer.WriteSafeString(dt.ToString(format));
-            });
-            HandlebarsDotNet.Handlebars.RegisterHelper("price", (writer, context, args) =>
-            {
-                string formated = args[0].GetType() switch
-                {
-                    {} d when d == typeof(double) => ((double)args[0]).ToString("0.00"),
-                    {} i when i == typeof(int) => ((int)args[0]).ToString("0.00"),
-                    {} dd when dd == typeof(decimal) => ((decimal)args[0]).ToString("0.00"),
-                    _ => args[0].ToString()
-                };
-                writer.WriteSafeString(formated);
-            });
+            LevisCompiler.RegisteHelpers();
         }
 
         [Benchmark(Baseline = true)]
